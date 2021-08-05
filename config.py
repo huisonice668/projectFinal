20e732fb338c2e802814362e7dd395f17dcfae92
zihui <zb4@gmail.com>
2021-08-05 13:34:00 -0400

    commit 6: step 6

--- config.py
+++ config.py
@@ -1,28 +1,22 @@
-"""Flask configuration."""
+"""App configuration."""
 from os import environ, path
+
 from dotenv import load_dotenv
 
+# Load variables from .env
 basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+load_dotenv(path.join(basedir, ".env"))
+
 
 
 class Config:
-    """Base config."""
-    SECRET_KEY = environ.get('SECRET_KEY')
-    SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
-    STATIC_FOLDER = 'static'
-    TEMPLATES_FOLDER = 'templates'
+    """Set Flask configuration vars from .env file."""
 
+    # General Config
+    SECRET_KEY = environ.get("SECRET_KEY")
+    FLASK_APP = environ.get("FLASK_APP")
+    FLASK_ENV = environ.get("FLASK_ENV")
 
-class ProdConfig(Config):
-    FLASK_ENV = 'production'
-    DEBUG = False
-    TESTING = False
-    DATABASE_URI = environ.get('PROD_DATABASE_URI')
-
-
-class DevConfig(Config):
-    FLASK_ENV = 'development'
-    DEBUG = True
-    TESTING = True
-    DATABASE_URI = environ.get('DEV_DATABASE_URI')
+    # Static Assets
+    STATIC_FOLDER = "static"
+    TEMPLATES_FOLDER = "templates"
